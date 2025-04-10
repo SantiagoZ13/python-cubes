1ad621762c786680062f1d5273568d60dabc4243
SantiagoZ13 <diazsantiagoponce1@gmail.com>
2025-04-10 17:21:54 -0500

    several fixes

--- tuits.py
+++ tuits.py
@@ -10,7 +10,7 @@ def calcular_bits_tuit(caracteres_posibles, limite_caracteres_tuit):
     return bits_por_caracter, capacidad_tuit, bits_tuit_total
 
 bits_caracter, capacidad, bits_total = calcular_bits_tuit(100, 100) 
-# Más acertado es poner como capacidad 280, pero para el ejemplo es más fácil poner 100
+# Más acertado es poner como capacidad 280, pero siguiendo el problema es 100 
 
 print(f"Bits por carácter: {bits_caracter}")
 print(f"Capacidad del tuit (caracteres): {capacidad}")
